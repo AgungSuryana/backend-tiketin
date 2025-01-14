598f02b1602162e3ff09b9532bb79de1f06aa4d4
AgungSuryana <agungsur84@gmail.com>
2025-01-14 23:17:47 +0700

    tt

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
     host: "bpu871t0hx7db2sl1v8u-mysql.services.clever-cloud.com",
     user: "uvhda4dehpdmuhys",
     password: "WpUZ18HSrgTr6bLsRiRn",
     database: "bpu871t0hx7db2sl1v8u",
-    port: 3306
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err.message);
-    } else {
-        console.log('Connected to the database!');
-    }
-});
-
-module.exports = db;
+module.exports = pool.promise();
